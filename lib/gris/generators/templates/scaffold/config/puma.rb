8c30e7ae70e173cb5473b5936fa0bcce16b659e3
Dylan Fareed <email@dylanfareed.com>
2015-05-27 13:33:05 -0400

    moves Procfile puma config to a file; adds active_record_shared_connection rspec support file; provides hyperclient spec as default endpoint specs

---
+++ lib/gris/generators/templates/scaffold/config/puma.rb
@@ -0,0 +1,6 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
