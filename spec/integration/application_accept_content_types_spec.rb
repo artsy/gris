22397fef8ed4cffd938f0bbf84d8cefe2cb819c7
Dylan Fareed <email@dylanfareed.com>
2016-08-29 12:19:02 -0400

    temporarily disabling integration specs

--- spec/integration/application_accept_content_types_spec.rb
+++ spec/integration/application_accept_content_types_spec.rb
@@ -4,7 +4,7 @@ require 'faraday'
 describe 'cascade api versions' do
   include_context 'with a generated app'
 
-  it 'returns a the default content_type' do
+  xit 'returns a the default content_type' do
     request = Faraday.get "http://localhost:#{app_port}/", {}, 'Accept' => 'vnd.bogus-v2+json'
     expect(request.to_hash[:response_headers]['content-type']).to eq(
       'application/hal+json'
